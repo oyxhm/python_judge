baa4afa9ef09d99112cc84e4036b03125b23089e
oyxhm <376841842@qq.com>
2014-06-04 18:22:45 +0800

    0

--- judgeyear.py
+++ judgeyear.py
@@ -1,18 +1,17 @@
+#coding=utf-8
 def judge(year):
+	try:
+		year = int(year)
+	except ValueError:
+		print "please Enter numbers !"
+		return
 	if int(year) % 4 == 0  and int(year) % 100 != 0 :
 		print "yes"
 	elif int(year) %400 == 0:
 		print "yes"
 	else :
 		print "no"
-def error(year):
-	flag =(year.isalpha()) or (not (year.isalpha() or year.isdigit()) and year.isalnum()) 
-	if flag == True :
-		print "bieluanshu!!!"
-	else :
-		judge(year)
-	
-	
-year = raw_input("shuruyigeshu:")
-error(year)
+
+year = raw_input("Enter a year to judge:")
+judge(year)
 
